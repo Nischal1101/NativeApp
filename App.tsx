482ec33a53eef0b83366e261429d8f2341dfb4ad
nischal1101 <kaflenk123@gmail.com>
2024-09-23 11:50:53 +0545

    start roll a dice project

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  useColorScheme,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
 
 function App() {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <SafeAreaView>
-      <View>
-        <Text>hi</Text>
-        <Text>hi</Text>
-        <Text>hi</Text>
-        <Text>hi</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.whiteText}>
+        Hello from react-native
+      </Text>
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {},
+  whiteText: {
+    color: '#ffffff',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
 export default App;
